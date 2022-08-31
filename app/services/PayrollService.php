ef1616f8f1d7911860239300c2870a970f3cdc5d
user06 <user06@vodex.local>
2022-08-31 15:06:45 +0200

    monthly hour

--- app/services/PayrollService.php
+++ app/services/PayrollService.php
@@ -8,12 +8,12 @@ use Carbon\Carbon;
 
 class PayrollService
 {
-    public function salaryPerHour(Employee $employee)
+    public function salaryPerHour($from, $to,$salary)
     {
-        $startTime = Carbon::parse($employee->shift->from);
-        $endTime = Carbon::parse($employee->shift->to);
+        $startTime = Carbon::parse($from);
+        $endTime = Carbon::parse($to);
         $totalWorkHour = intval(gmdate('H',$endTime->diffInSeconds($startTime))) * 30;
-        $salaryBerHour = $employee->salary / $totalWorkHour;
+        $salaryBerHour = $salary / $totalWorkHour;
         return $salaryBerHour;
     }
 
@@ -39,9 +39,23 @@ class PayrollService
 
 
 
-    public function totalHourPerMonth(Attendance $attendance)
+    public function totalHourPerMonth($work_hour ,$month)
     {
-//        $data = $attendance->
+        $yearData =  Attendance::selectRaw('year(created_at) as Year, monthname(created_at) as Month, count(id) as Trips')
+            ->groupBy('Year','Month')
+            ->orderByRaw('min(created_at) desc')
+            ->get();
+        $years = $yearData->pluck('Year')->toArray();
+
+        $data['Monthly'] =  Attendance::selectRaw('year(created_at) as Year, monthname(created_at) as Month, count(id) as Trips')
+            ->groupBy('Year','Month')
+            ->orderByRaw('min(created_at) desc')
+            ->get();
+        $data['Monthly']->map(function ($item){
+            $item->hour = Attendance::whereMonth('created_at', date('m',strtotime($item->Month)))
+                ->whereYear('created_at',(String)$item->Year)->sum('work_hour');
+        });
+        return $data;
     }
 
     public function totalSalaryPerHour()
